65394e8167d48ce7936cc806e8109661e1554d2f
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-11-09 18:07:39 +0700

    Change Route on /me

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -8,5 +8,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
-    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
