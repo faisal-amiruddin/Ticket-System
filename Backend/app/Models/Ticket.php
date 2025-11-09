94cc5cf7ade8e0ef24cb6233d2844896c83a25d9
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-11-09 16:40:40 +0700

    Add Ticket Reply Model and Migration with Relations

--- Backend/app/Models/Ticket.php
+++ Backend/app/Models/Ticket.php
@@ -19,4 +19,9 @@ class Ticket extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function ticketReplies()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
